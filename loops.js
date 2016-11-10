985f46cc667e576493ff3ffe4cd27f94bd547486
kurt spencer <KurtSpencer@kurts-MacBook-Pro.local>
2016-11-09 22:01:14 -0700

    initial push

---
+++ loops.js
@@ -0,0 +1,33 @@
+
+for (var i = 0; i <= 500; i += 100) {
+	console.log([i]);
+};
+
+for (var i = 0; i < 7; i++) {
+	console.log(Math.pow(2,[i]));
+	};
+
+for (var i = 1; i < 4; i++) {
+	console.log([i] + [i] + [i])
+};
+
+for (var i = 1; i < 4; i++) {
+	console.log(i + i + i)
+};
+
+for (var i = 0; i < 11; i+=2) {
+	console.log(i);
+};
+
+for (var j = 3; j < 16; j += 3){
+    console.log(j);
+};
+
+for (var j = 9; j >= 0 ; j--){
+    console.log(j);
+};
+
+for(var z = 0; z<3; z++){
+ for(var j=0; j<4; j++){
+    console.log(j)}
+}
